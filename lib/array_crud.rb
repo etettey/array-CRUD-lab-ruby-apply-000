3b8e613ef9883003755d205b4057f2a301aaaf3e
resourceful-role-9993 <unicetee@gmail.com>
2020-03-17 02:03:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,38 @@
-def create_an_empty_array
-  
+ def create_an_empty_array
+  arr=[]
+
 end
 
 def create_an_array
-  
+arr=["dad","BT","O.B","Kiki"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array << element
 end
+add_element_to_end_of_array(array=["dad","BT","O.B","Kiki], "Sisi")
+
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
